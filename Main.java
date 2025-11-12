d8dc69b954b811377743e5a03503772e4cab915e
Kishor S <kishorsatheeskumar@gmail.com>
2025-11-12 12:12:59 +0530

    Add calculator functionality to Main class

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins! Your CI pipeline is running successfully.");
+        int result = Calculator.add(5, 10);
+        System.out.println("Addition Result: " + result);
     }
 }
