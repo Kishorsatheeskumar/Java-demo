eb65e92b65e15cde07813c7a5b1d740aae7d39a9
Kishor S <kishorsatheeskumar@gmail.com>
2025-11-12 12:03:49 +0530

    Rename HelloJenkins class to Main

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class HelloJenkins {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins! Your CI pipeline is running successfully.");
     }
