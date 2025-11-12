454057a55059aaed0819bcf928d97efd868629b3
Kishor S <kishorsatheeskumar@gmail.com>
2025-11-12 12:11:31 +0530

    Remove main method from Calculator class
    Removed the main method from the Calculator class.

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,4 @@ public class Calculator {
     public static int add(int a, int b) {
         return a + b;
     }
-
-    public static void main(String[] args) {
-        int result = add(5, 10);
-        System.out.println("Addition Result: " + result);
-    }
 }
